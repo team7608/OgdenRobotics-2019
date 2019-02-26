de1059014059cd29f5d8db602eede81412ae8703
oscaaa21 <kingoscar2002@gmail.com>
2019-02-26 16:39:41 -0600

    t

---
+++ 7608 Test/src/main/java/org/usfirst/frc/team7608/robot/OI.java
@@ -0,0 +1,120 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team7608.robot;
+import edu.wpi.first.wpilibj.Joystick;
+//import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import org.usfirst.frc.team7608.robot.commands.MoveHatchPannelDownCommand;
+import org.usfirst.frc.team7608.robot.commands.MoveHatchPannelUpCommand;
+
+
+
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+
+	//public static Joystick mainJoystick;
+	public static Joystick secondJoystick;
+	public static JoystickButton a = new JoystickButton(secondJoystick, 1);
+	public static JoystickButton b = new JoystickButton(secondJoystick, 2);
+	public static JoystickButton x = new JoystickButton(secondJoystick, 3);
+	public static JoystickButton y = new JoystickButton(secondJoystick, 4);
+
+
+
+	public OI() {
+		a.whenPressed(new MoveHatchPannelUpCommand());
+		b.whenPressed(new MoveHatchPannelDownCommand());
+		
+
+	
+	//mainJoystick = new Joystick(RobotMap.mainStick);
+	secondJoystick = new Joystick(RobotMap.secondStick){
+	
+		// @Override
+		// public double getY(Hand hand) {
+		// 	return 0;
+		// }
+	
+		// @Override
+		// public double getX(Hand hand) {
+		// 	return 0;
+		// }
+	
+		@Override
+		public void setRumble(RumbleType type, double value) {
+			
+		}
+	
+		@Override
+		public void setOutputs(int value) {
+			
+		}
+	
+		@Override
+		public void setOutput(int outputNumber, boolean value) {
+			
+		}
+	
+		@Override
+		public HIDType getType() {
+			return null;
+		}
+	
+		// @Override
+		// public boolean getStickButton(Hand hand) {
+		// 	return false;
+		// }
+	
+		@Override
+		public boolean getRawButton(int button) {
+			return false;
+		}
+	
+		@Override
+		public double getRawAxis(int axis) {
+			return 0;
+		}
+	
+		@Override
+		public int getPOVCount() {
+			return 0;
+		}
+	
+		@Override
+		public int getPOV(int pov) {
+			return 0;
+		}
+	
+		@Override
+		public String getName() {
+			return null;
+		}
+	
+		// @Override
+		// public boolean getBumper(Hand hand) {
+		// 	return false;
+		// }
+	};
+	
+	//rightJoystick = new Joystick(RobotMap.rStick);
+	
+	//new JoystickButton(OI.leftJoystick, 0).whenPressed(new DriveCommand());
+	//new JoystickButton(OI.rightJoystick, 1).whenPressed(new DriveCommand());
+//     public Joystick partnerController = new Joystick(2);
+//     public JoystickButton partnerButtonX = new JoystickButton(partnerController, 1);
+//     public JoystickButton partnerButtonB = new JoystickButton(partnerController, 3);
+//     public JoystickButton partnerButtonA = new JoystickButton(partnerController, 2);
+// public JoystickButton partnerButtonY = new JoystickButton(partnerController, 4);
+
+
+	}
+}
---
+++ 7608 Test/src/main/java/org/usfirst/frc/team7608/robot/commands/MoveHatchPannelUpCommand.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team7608.robot.commands;
+
+import org.usfirst.frc.team7608.robot.OI;
+import org.usfirst.frc.team7608.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class MoveHatchPannelUpCommand extends Command {
+  
+  public MoveHatchPannelUpCommand() {
+  requires(Robot.hatchSubsystem);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    System.out.println("Execute Up");
+   // if (OI.secondJoystick.getRawAxis(1) > (0.1)) 
+     Robot.hatchSubsystem.HatchPannelLiftUp();
+   
+  }
+  
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
